b4f401c825525f501d6ee8f438c2e7b77f02c9d5
Yasin Arslan <yyasinarslan@gmail.com>
2017-03-15 21:32:36 +0300

    TEMP COMMIT

--- app/src/main/java/com/getirhackathon/preselection/utils/Constants.java
+++ app/src/main/java/com/getirhackathon/preselection/utils/Constants.java
@@ -3,6 +3,7 @@ package com.getirhackathon.preselection.utils;
 public class Constants {
 
     public static String URL = "https://getir-bitaksi-hackathon.herokuapp.com/getElements";
+    public static String URL_RETROFIT = "https://getir-bitaksi-hackathon.herokuapp.com/";
 
     public static String TYPE_CIRCLE = "circle";
     public static String TYPE_RECTANGLE = "rectangle";
